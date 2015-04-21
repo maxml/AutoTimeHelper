577dadcbddd3fccd6cd27de5a19b83df9eb3a768
Kizama <dmitrikkizama@gmail.com>
2015-04-21 18:37:21 +0300

    ParseRegistration

---
+++ ex/DataBaseDemo/src/com/demo/databasedemo/AppConstants.java
@@ -0,0 +1,9 @@
+package com.demo.databasedemo;
+
+public class AppConstants {
+
+	public static final String BASE = "BaseExample";
+	public static final String EMAIL = "EmailExample";
+	public static final String NAME = "NameExample";
+	public static final String MY_TAG = "My boolshit";
+}
