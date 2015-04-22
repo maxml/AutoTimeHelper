71c125c17d72439d67eacd06fcd54f64623a73ce
rLantar <rlantar@gmail.com>
2015-04-22 21:19:27 +0300

    ListView
    ListView
    enum type, Slice, description, time difference
    collor type

---
+++ ex/ListViewTimeHelp/src/entity/Table.java
@@ -0,0 +1,26 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class Table {
+	private ArrayList<Slice> list;
+
+	public Table(ArrayList<Slice> list) {
+		super();
+		this.list = list;
+	}
+
+	public ArrayList<Slice> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<Slice> list) {
+		this.list = list;
+	}
+
+	@Override
+	public String toString() {
+		return "Table [list=" + list + "]";
+	}
+
+}
