06c04d4053d15b2d652d7b65a8cf93e7c6fd1bd3
rLantar <rlantar@gmail.com>
2015-04-22 16:05:16 +0300

    Login
    Authorization
    User - login password email
    Sign In
    forgot password

---
+++ ex/Autorization/src/com/example/autorization/Main.java
@@ -0,0 +1,15 @@
+package com.example.autorization;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lantar on 22.04.2015.
+ */
+public class Main extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
