9850754e05e2fc5ef4f25c71791fa67d80b54e31
Kizama <dmitrikkizama@gmail.com>
2015-04-30 19:02:45 +0300

    gitignore

--- project/MainProject/src/com/maxml/timer/OnStart.java
+++ project/MainProject/src/com/maxml/timer/OnStart.java
@@ -1,38 +1,21 @@
 package com.maxml.timer;
 
-import java.util.Date;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.provider.ContactsContract.Data;
-import android.util.Log;
 
 import com.maxml.timer.activity.authorization.LoginActivity;
-import com.maxml.timer.api.PointCRUD;
-import com.maxml.timer.api.SliceCRUD;
-import com.maxml.timer.entity.Line;
-import com.maxml.timer.entity.Point;
-import com.maxml.timer.entity.Slice;
-import com.maxml.timer.entity.Slice.SliceType;
-import com.parse.Parse;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
 
 public class OnStart extends Activity {
-	
+
 	private DeviceHandler mHandler;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// setContentView(R.layout.activity_main);
-		Parse.enableLocalDatastore(this);
-
-		Parse.initialize(this, "v7xyeLy701EXeS70aggBiMDzRIlTH0q1tiYlZqFV",
-				"oxzucbjTHAKP20t9o4UjBCRBoKN0gk4jL4Mm9UjK");
-		ParseInstallation.getCurrentInstallation().saveInBackground();
 
 		mHandler = new DeviceHandler();
 
@@ -40,9 +23,8 @@ public class OnStart extends Activity {
 		startActivity(intent);
 		finish();
 
-
 	}
-	
+
 	class DeviceHandler extends Handler {
 
 		@Override
@@ -50,8 +32,6 @@ public class OnStart extends Activity {
 			// TODO Auto-generated method stub
 			super.handleMessage(msg);
 		}
-		
-		
-		
+
 	}
 }
