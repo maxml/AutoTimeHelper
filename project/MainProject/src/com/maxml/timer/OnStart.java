1c61937b5fe6d92e8aaaea86cc6306f304eb1f69
Dima <muzey84@gmail.com>
2015-04-30 19:46:04 +0300

    fix

--- project/MainProject/src/com/maxml/timer/OnStart.java
+++ project/MainProject/src/com/maxml/timer/OnStart.java
@@ -16,14 +16,6 @@ public class OnStart extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-		/*Parse.initialize(this, "v7xyeLy701EXeS70aggBiMDzRIlTH0q1tiYlZqFV",
-				"oxzucbjTHAKP20t9o4UjBCRBoKN0gk4jL4Mm9UjK");
-		ParseInstallation.getCurrentInstallation().saveInBackground();
-		Parse.enableLocalDatastore(this);*/
-=======
->>>>>>> 872c8cbaee1aa9d3e3490b3722889c485b4589e5
 
 		mHandler = new DeviceHandler();
 
