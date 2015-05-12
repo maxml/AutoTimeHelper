6ce992276f36fc197e2bec7b342616966ce02962
Kizama <dmitrikkizama@gmail.com>
2015-05-12 17:41:30 +0300

    added class NetworkAvailiability

---
+++ project/MainProject/src/com/maxml/timer/api/NetworkAvailiabilityHelper.java
@@ -0,0 +1,36 @@
+package com.maxml.timer.api;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class NetworkAvailiabilityHelper {
+
+	private static boolean networkDataBaseAvailable = true;
+	
+	
+	public void init(Context context){
+		IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);        
+		context.registerReceiver(networkStateReceiver, filter);
+	}
+	
+	public static boolean getIsNetworkAvailable() {
+		return networkDataBaseAvailable;
+	}
+
+	public static void setIsNetworkAvailable(boolean s) {
+		networkDataBaseAvailable = s;
+	}
+
+	private BroadcastReceiver networkStateReceiver = new BroadcastReceiver() {
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+	        Log.w("Network Listener", "Network Type Changed");
+
+
+	    }
+	};
+}
