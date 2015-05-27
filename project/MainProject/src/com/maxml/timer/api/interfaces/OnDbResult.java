27bd7748b961a1b0c3c9131e48c32ba9754eab45
Lantar <rlantar@gmail.com>
2015-05-27 16:50:37 +0300

    update interfece

--- project/MainProject/src/com/maxml/timer/api/interfaces/OnDbResult.java
+++ project/MainProject/src/com/maxml/timer/api/interfaces/OnDbResult.java
@@ -2,13 +2,12 @@ package com.maxml.timer.api.interfaces;
 
 import java.util.List;
 
+import com.maxml.timer.entity.Line;
 import com.maxml.timer.entity.Point;
 import com.maxml.timer.entity.Slice;
-import com.maxml.timer.entity.User;
 import com.parse.ParseObject;
 
 public interface OnDbResult {
-<<<<<<< HEAD
 
 	void onResult(ParseObject parseLine, Slice slice);
 
@@ -18,15 +17,4 @@ public interface OnDbResult {
 
 	void onResult(Point point, List<Slice> sliceList);
 
-=======
-	
-	void onResult(Point point, List<Slice> sliceList);
-	
-	void onResult(ParseObject parsePoint, Slice slice);
-	
-	void onResultRead(List<Slice> sliceList);
-	
-	void onResult(ParseObject parsePoint);
-	
->>>>>>> 02b27d3eca81262aed00546e5ae8d7972d812680
 }
